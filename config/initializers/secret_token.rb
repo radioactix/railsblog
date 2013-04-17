32efb860bc4e9729836ada062b2daa22878dcc0f
unknown <raum103@raum103-14.LUO.local>
2013-04-17 08:17:16 +0200

    Posts-Datenbank, Validierung, Layout-Farbe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '698d1a72d54ce9d8a9ca649015c7e1f80f07941f2fe0739bd6634a64af81bbce54f7be04d730c1a6dc3ee02adebd6129e441b44388d052e13b2c083961d0e9a4'
